3e59a7354f9c58443982a3ed31475b2bea65e370
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-19 15:05:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
@@ -2,15 +2,16 @@ class Solution {
   public:
     bool divby13(string &s) 
     {
-        int n=s.size();
-        int num=0;
-        for(int i=0;i<n;i++)
-        {
-            int rem=s[i]-'0';
-            num=num*10+rem;
-            if(num>=13)
-            num=num%13;
-        }
-        return num==0;
+       int n=s.size();
+       int rem=0;
+       for(int i=0;i<n;i++)
+       {
+           int digit=s[i]-'0';
+           rem=rem*10+digit;
+           rem=rem%13;
+    
+       }
+       return rem==0;
+       
     }
 };
