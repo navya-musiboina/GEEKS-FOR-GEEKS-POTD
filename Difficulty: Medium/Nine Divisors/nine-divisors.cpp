7e2276413ae72252a6f8c4f5608d6ebe8df20810
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-19 15:25:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nine Divisors/nine-divisors.cpp
+++ Difficulty: Medium/Nine Divisors/nine-divisors.cpp
@@ -1,34 +1,30 @@
 class Solution {
   public:
   
-  
-  bool has9div(int x){
-      // finding divs of the numbers. 
-      int cnt = 0;
-      
-      for(int i=1; i*i<=x; i++){
-          if(x%i==0 && x!=i*i) cnt+=2;
-          else if(x%i==0) cnt++;
-          if(cnt>9) return false;
-      }
-      
-      return cnt==9;
-      
-  }
-  
-  
-    int countNumbers(int n) {
-        // Code Here
-        int ans = 0;
-        
-        // as from example 36 is the first one. 
-        for(int i=6; i*i<=n; i++){
-            if(has9div(i*i)){
-                ans++;
-            }
+
+    int check(int x)
+    {
+        int count=0;
+        for(int i=1;i<=sqrt(x);i++)
+        {
+            if(x%i==0  && x/i!=i)count+=2;
+            else if(x%i==0)count++;
+            if(count>9)
+            return false;
         }
-        return ans;
-        
+        if(count==9)
+        return true;
+        return false;
+    }
+    int countNumbers(int n) 
+    {
+        int count=0;
+        for(int i=6;i<=sqrt(n);i++)
+        {
+            if(check(i*i))
+            count++;
+        }
+        return count;
         
     }
 };
