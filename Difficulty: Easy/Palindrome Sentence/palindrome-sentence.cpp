acea22cdaec5d734d8426b74f58ba5dcdad564f5
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-05 08:46:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Sentence/palindrome-sentence.cpp
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.cpp
@@ -1,23 +1,22 @@
-// User template code in C++
-
 class Solution {
   public:
-    bool sentencePalindrome(string &s) 
+    bool isPalinSent(string &s)
     {
         int n=s.size();
-       string res="";
-       for(char ch:s)
-       {
-           if(isalnum(ch))
-           res+=tolower(ch);
-       }
-       int i=0,j=res.size()-1;
-       while(i<j)
-       {
-           if(res[i]!=res[j])
-           return false;
-           i++;j--;
-       }
-       return true;
+        string ans;
+        for(char ch:s)
+        {
+            if(isalnum(ch))
+            ans+=tolower(ch);
+        }
+        int i=0,j=ans.size()-1;
+        while(i<=j)
+        {
+            if(ans[i]!=ans[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+        
     }
 };
