784335da7de922c3ad44387e827cc74fc8aad90a
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-06 19:45:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Min Cost to Make Size 1/min-cost-to-make-size-1.cpp
+++ Difficulty: Easy/Min Cost to Make Size 1/min-cost-to-make-size-1.cpp
@@ -5,9 +5,7 @@ class Solution {
     int cost(vector<int>& arr) 
     {
         int n=arr.size();
-        int ans=0;
         int mini=*min_element(arr.begin(),arr.end());
         return mini*(n-1);
-        
     }
 };
