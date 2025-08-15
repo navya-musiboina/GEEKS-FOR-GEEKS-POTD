9899e3b20ea9adaa542cbeccb945b4031966a230
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-15 10:39:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,25 +1,27 @@
 class Solution {
-  public:
-    int func(vector<int>&arr,int i,int k,vector<vector<int>>&dp)
+public:
+    int func(vector<int>& arr, int i, int k, vector<vector<int>>& dp)
     {
-        if(i>=arr.size())
-        {
-            return k==0;
-        }
+      
+        if(i>=arr.size()) return k==0;
+        int take=0,ntake=0;
         if(dp[i][k]!=-1) return dp[i][k];
-        int lh=0,rh=0;
         if(k-arr[i]>=0)
-        lh=func(arr,i+1,k-arr[i],dp);
-        rh=func(arr,i+1,k,dp);
-        return dp[i][k]=lh+rh;
+        take=func(arr,i+1,k-arr[i],dp);
+        ntake=func(arr,i+1,k,dp);
+        return dp[i][k]=take+ntake;
+
     }
+
     int countPartitions(vector<int>& arr, int d) 
     {
-         int n=arr.size();
-        int sum=accumulate(arr.begin(),arr.end(),0);
-        if((sum-d)<0 || (sum-d)%2!=0) return 0;
-        int k=(sum-d)/2;
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return func(arr,0,k,dp);
+        int n=arr.size();
+        int t=accumulate(arr.begin(),arr.end(),0);
+        int sum=(t-d)/2;
+        if((t-d)<0  || (t-d)%2!=0) return 0;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        
+        return func(arr,0,sum,dp);
+    
     }
 };
