4943839426bb84b539440551e03fa9660e86d6b5
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-16 21:39:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -3,25 +3,25 @@ class Solution {
    
     int findPlatform(vector<int>& arr, vector<int>& dep) 
     {
-     int  n=arr.size();
-     sort(arr.begin(),arr.end());
-     sort(dep.begin(),dep.end());
-     int i=0,count=0,maxi=0,j=0;
-     while(i<n && j<n)
-     {
-         if(arr[i]<=dep[j])
-         {
-             count++;
-             i++;
-         }
-         else
-         {
-             j++;
-             count--;
-         }
-         maxi=max(maxi,count);
-     }
-     return maxi;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int i=0,j=0,maxi=0,count=0;
+        while(i<arr.size() && j<dep.size())
+        {
+            if(arr[i]<=dep[j])
+            {
+                count++;
+                i++;
+                maxi=max(maxi,count);
+            }
+            else
+            {
+                count--;
+                j++;
+            }
+        }
+        return maxi;
     }
 };
 
